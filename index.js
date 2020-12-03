57b2cbf472e6f17cd3b21c58a5926eb77d543bf9
Doug Schallmoser <dougschallmoser@gmail.com>
2020-12-03 03:19:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
 // Code your solution here!
 
+function printString(string) {
+  console.log(string[0])
+  
+  if (string.length > 1) {
+    let subString = string.substring(1, string.length)
+    printString(subString)
+  } else {
+    return true
+  }
+}
+
+function reverseString(string) {
+  if (string.length > 0) {
+    let subString = string.substring(0, string.length - 1)
+    reverseString(subString)
+  } else {
+    return true
+  }
+}
+
+function reverseString(myString) {
+  if (myString.length < 2) {
+    return myString;
+  } else {
+    return reverseString(myString.substring(1)) + myString[0];
+  }
+}
