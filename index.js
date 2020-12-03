d03f9294bf526d8fad51c4279055ada8eb8f816b
Doug Schallmoser <dougschallmoser@gmail.com>
2020-12-03 03:26:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,5 +24,5 @@ function isPalindrome(string) {
     return true
   } else if (string[0] === string[string.length - 1]) {
     return isPalindrome(string.substring(1, string.length - 1))
-  }
+  } else {
 }
